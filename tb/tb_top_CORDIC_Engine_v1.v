332f1ffbcc62af6c5b27629057ced451bcce38a1
Kasetty-Praveen-Kumar <kpkkumar.1991@gmail.com>
2025-09-01 15:55:05 +0530

    Modified the testbench for top_CORDIC_Engine module
    Signed-off-by: Kasetty-Praveen-Kumar <kpkkumar.1991@gmail.com>

--- tb/tb_top_CORDIC_Engine_v1.v
+++ tb/tb_top_CORDIC_Engine_v1.v
@@ -19,12 +19,11 @@ module tb_top_CORDIC_Engine_v1();
     wire signed [DATA_WIDTH - 1 : 0] out_alpha;
     wire o_valid_out;
 
-
     initial i_clk = 1'b1;
     always #5 i_clk = ~i_clk;
 
-    localparam DATA_WIDTH = 18;
-    localparam N_PE = 15;
+    localparam DATA_WIDTH = 16;
+    localparam N_PE = 13;
 
     top_CORDIC_Engine_v1 # (
         .DATA_WIDTH(DATA_WIDTH),
@@ -44,6 +43,9 @@ module tb_top_CORDIC_Engine_v1();
       .o_valid_out(o_valid_out)
     );
 
+    real absolute_error_cos_theta = 0;
+    real absolute_error_sin_theta = 0;
+
     // alpha is the angle in radians, represented in fixed-point format
     integer f1,f2,f3;
     initial begin
@@ -51,7 +53,7 @@ module tb_top_CORDIC_Engine_v1();
         $dumpfile("tb_top_CORDIC_Engine_v1.vcd");
         $dumpvars(0);
 
-        f1 = $fopen("input.txt", "w");
+        f1 = $fopen("input_angle.txt", "r");
         f2 = $fopen("cos_output.txt", "w");
         f3 = $fopen("sin_output.txt", "w");
 
@@ -67,32 +69,19 @@ module tb_top_CORDIC_Engine_v1();
         in_atan_0 = 0;
 
         #10 i_rst_n = 1'b1;
-        #10 i_valid_in = 1'b1;
-            in_x = 18'h026de; // Scaling value for CORDIC 0.60729
-            in_y = 18'h00000;
-            in_alpha = 18'h00000;
-            in_atan_0 = 18'h03244;
-        // #10 in_alpha = 18'h03424;
-        // #10 in_alpha = 18'h02300;
-        // #10 in_alpha = 18'h039f8;
-        // #10 in_alpha = 18'hfc99a;
-        // #10 in_alpha = 18'hfd000;
-        // #10 in_alpha = 18'h03244; 
-        // #10 in_alpha = 18'hfcdbc;
-        // #10 in_alpha = 18'h08000;
-        // #10 in_alpha = 18'h0cccd;
-        // #10 in_alpha = 18'h0f99a;
-        // #10 in_alpha = 18'h13333;
-        // #10 in_alpha = 18'h06487;
-        // #10 in_alpha = 18'h0c909;
-        // #10 in_alpha = 18'h12d97;
-        #10 i_valid_in = 1'b0;
-        wait(o_valid_out) // Wait for the CORDIC computation to finish
-        #10 i_valid_in = 1'b1;
-            in_alpha   = 18'h03424;
-        #10 i_valid_in = 1'b0;
-        wait(o_valid_out)
-        #40;
+            in_x = 16'h09b7; // Scaling value for CORDIC 0.60729
+            in_y = 16'h0000;
+            in_atan_0 = 16'h0c91;
+        
+        while(!$feof(f1)) begin
+            i_valid_in = 1'b1;
+            $fscanf(f1, "%b\n", in_alpha);
+            #10 i_valid_in = 1'b0;
+            wait(o_valid_out) // Wait for the CORDIC computation to finish
+            $fwrite(f2, "%h\n", out_costheta);
+            $fwrite(f3, "%h\n", out_sintheta);
+        end
+        
         $fclose(f1);
         $fclose(f2);
         $fclose(f3);
@@ -100,7 +89,7 @@ module tb_top_CORDIC_Engine_v1();
         $display("Simulation finished successfully.\n");
         $display("Analyzing results...");
 
-        f1 = $fopen("input.txt", "r");
+        f1 = $fopen("input_angle.txt", "r");
         f2 = $fopen("cos_output.txt", "r");
         f3 = $fopen("sin_output.txt", "r");
 
@@ -109,29 +98,32 @@ module tb_top_CORDIC_Engine_v1();
             reg signed [DATA_WIDTH - 1 : 0] r_out_costheta;
             reg signed [DATA_WIDTH - 1 : 0] r_out_sintheta;
 
-            $fscanf(f1, "%h\n", r_in_alpha);
+            $fscanf(f1, "%b\n", r_in_alpha);
             $fscanf(f2, "%h\n", r_out_costheta);
             $fscanf(f3, "%h\n", r_out_sintheta);
 
             $display("--------------------------------------------");
-            $display("Input angle    : dec=%0d hex=%h -> rad=%f", r_in_alpha, r_in_alpha, r_in_alpha / (2**14.0));
-            $display("Expected       : cos=%f sin=%f", $cos(r_in_alpha/2**14.0), $sin(r_in_alpha/2**14.0));
-            $display("CORDIC Cosine  : dec=%0d hex=%h -> val=%f", r_out_costheta, r_out_costheta, r_out_costheta / (2**14.0));
-            $display("CORDIC Sine    : dec=%0d hex=%h -> val=%f\n", r_out_sintheta, r_out_sintheta, r_out_sintheta / (2**14.0));
+            $display("Input angle    : dec=%0d hex=%h -> rad=%f", r_in_alpha, r_in_alpha, r_in_alpha / (2**12.0));
+            $display("Expected       : cos=%f sin=%f", $cos(r_in_alpha/2**12.0), $sin(r_in_alpha/2**12.0));
+            $display("CORDIC Cosine  : dec=%0d hex=%h -> val=%f", r_out_costheta, r_out_costheta, r_out_costheta / (2**12.0));
+            $display("CORDIC Sine    : dec=%0d hex=%h -> val=%f\n", r_out_sintheta, r_out_sintheta, r_out_sintheta / (2**12.0));
+
+            /* ------------------- Calculation of Error between Expected and Computed Values --------------------- */
+            if(($cos(r_in_alpha/2**12.0) - r_out_costheta / (2**12.0)) >= 0)
+                absolute_error_cos_theta = absolute_error_cos_theta + ($cos(r_in_alpha/2**12.0) - r_out_costheta / (2**12.0));
+            else
+                absolute_error_cos_theta = absolute_error_cos_theta + (r_out_costheta / (2**12.0) - $cos(r_in_alpha/2**12.0));
+
+            if(($sin(r_in_alpha/2**12.0) - r_out_sintheta / (2**12.0)) >= 0)
+                absolute_error_sin_theta = absolute_error_sin_theta + ($sin(r_in_alpha/2**12.0) - r_out_sintheta / (2**12.0));
+            else
+                absolute_error_sin_theta = absolute_error_sin_theta + (r_out_sintheta / (2**12.0) - $sin(r_in_alpha/2**12.0));
         end
 
+        /* --------------- Mean Absolute Error --------------- */
+        $display("MAE_Costheta = %f\n", absolute_error_cos_theta/1000);
+        $display("MAE_sintheta = %f\n", absolute_error_sin_theta/1000);
         $finish;
-    end
-
-    // Write input and output values to files
-    always @(posedge i_clk) begin
-        if (i_valid_in) begin
-            $fwrite(f1, "%h\n", in_alpha);
-        end
-        if (o_valid_out) begin
-            $fwrite(f2, "%h\n", out_costheta);
-            $fwrite(f3, "%h\n", out_sintheta);
-        end
     end
 
 endmodule
